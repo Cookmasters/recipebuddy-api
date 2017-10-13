dcdd2111b55321ccfac214936916082ad20bbed1
KOUTOU Odilon <odilonkoutou@yahoo.fr>
2017-10-13 09:58:43 +0800

    Recipe class added

---
+++ lib/recipe.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: false
+
+require_relative 'page.rb'
+
+module RecipeBuddy
+  # Model for Recipe
+  class Recipe
+    def initialize(recipe_data, data_source)
+      @recipe = recipe_data
+      @data_source = data_source
+    end
+
+    def created_time
+      @recipe['created_time']
+    end
+
+    def content
+      @recipe['content']
+    end
+
+    def id
+      @recipe['id']
+    end
+  end
+end
