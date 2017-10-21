07847ca29ddf3625ddfc40e4c140387ad87e0ce3
KOUTOU Odilon <odilonkoutou@yahoo.fr>
2017-10-21 11:35:43 +0800

    HTTP errors module moved to a dedicated file

---
+++ lib/errors.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: false
+
+module Errors
+  # Not allowed to access resource
+  Unauthorized = Class.new(StandardError)
+  # Requested resource not found
+  NotFound = Class.new(StandardError)
+  # Bad request
+  BadRequest = Class.new(StandardError)
+end
